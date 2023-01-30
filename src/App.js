490f67083a0c3808a75f02301f844b11ea5483b0
karl065 <karlos065@hotmail.com>
2023-01-30 16:05:35 -0500

    Formato

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import CrearCuenta from "./componentes/CrearCuenta";
 import Login from "./componentes/Login";
 
@@ -7,8 +7,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" exact element= {<Login/>}/>
-        <Route path="/crear-cuenta" exact element= {<CrearCuenta/>}/>
+        <Route path="/" exact element={<Login />} />
+        <Route path="/crear-cuenta" exact element={<CrearCuenta />} />
       </Routes>
     </Router>
   );

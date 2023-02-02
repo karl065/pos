dc1f17801e03ea1c04c764a82d601ea0901ed5b6
karl065 <karlos065@hotmail.com>
2023-02-02 16:54:50 -0500

    Crear front login y crear cuentas

--- src/componentes/CrearCuenta.js
+++ src/componentes/CrearCuenta.js
@@ -3,17 +3,47 @@ import { Link } from "react-router-dom";
 
 const CrearCuenta = () => {
     return (
-        <div>
-            <h1>Crear Cuenta</h1>
-            <h2>Ingrese sus Datos</h2>
-            <input placeholder="Nombre" />
-            <input placeholder="Email" />
-            <input placeholder="Contraseña" />
-            <input placeholder="Confirmar" />
+        <main className="container mx-auto  mt-5 md:mt-20 p-5 md:flex md:justify-center">
+            <div className="md:w-2/3 lg:w-2/5">
+                <h1 className="incline bg-gradient-to-r from-indigo-200 via-blue-600 to-indigo-200 bg-clip-text font-display text-5xl tracking-tight text-transparent">Bienvenidos al POS</h1>
+                <h1 className="incline bg-gradient-to-r from-indigo-200 via-blue-600 to-indigo-200 bg-clip-text font-display text-5xl tracking-tight text-transparent">Iniciar Sesion</h1>
+                <form className="my-10 bg-white shadow rounded-lg p-10">
+                    <div className="my-5">
+                        <label className="uppercase text-gray-600 block text-lx font-bold">Nombre</label>
+                        <input
+                            type="nombre"
+                            placeholder="Ingrese su nombre"
+                            className="w-full mt-3 p-3 border rounded-xl bg-gray-50" />
+                        <label className="uppercase text-gray-600 block text-lx font-bold">Email</label>
+                        <input
+                            type="email"
+                            placeholder="Email"
+                            className="w-full mt-3 p-3 border rounded-xl bg-gray-50" />
+                        <label className="uppercase text-gray-600 block text-lx font-bold">Password</label>
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            className="w-full mt-3 p-3 border rounded-xl bg-gray-50" />
+                        <label className="uppercase text-gray-600 block text-lx font-bold">Confirmar Password</label>
+                        <input
+                            type="password"
+                            placeholder="Confirmar Password"
+                            className="w-full mt-3 p-3 border rounded-xl bg-gray-50" />
+                    </div>
+                    <input
+                        type="submit"
+                        value="Crear Cuenta"
+                        className="bg-blue-900 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-zinc-800 transition-colors" />
 
-            <button>Crear Cuenta</button>
-            <Link to={"/"}>Atras</Link>
-        </div>
+                    <Link
+                        className="block text-center my-5"
+                        to={"/"}
+                    >
+                        Atras
+                    </Link>
+                </form>
+            </div>
+        </main>
     );
 }
 
--- src/componentes/Login.js
+++ src/componentes/Login.js
@@ -3,15 +3,37 @@ import { Link } from "react-router-dom";
 
 const Login = () => {
     return (
-        <div>
-            <h1>Bienvenidos al POS</h1>
-            <h1>Iniciar Sesion</h1>
-            <h2>Ingrese sus Credenciales</h2>
-            <input placeholder="Email" />
-            <input placeholder="Contraseña" />
-            <button>Ingresar</button>
-            <Link to={"/crear-cuenta"}>Crear Cuenta</Link>
-        </div>
+        <main className="container mx-auto  mt-5 md:mt-20 p-5 md:flex md:justify-center">
+            <div className="md:w-2/3 lg:w-2/5">
+                <h1 className="incline bg-gradient-to-r from-indigo-200 via-blue-600 to-indigo-200 bg-clip-text font-display text-5xl tracking-tight text-transparent">Bienvenidos al POS</h1>
+                <h1 className="incline bg-gradient-to-r from-indigo-200 via-blue-600 to-indigo-200 bg-clip-text font-display text-5xl tracking-tight text-transparent">Iniciar Sesion</h1>
+                <form className="my-10 bg-white shadow rounded-lg p-10">
+                    <div className="my-5">
+                        <label className="uppercase text-gray-600 block text-lx font-bold">Email</label>
+                        <input
+                            type="email"
+                            placeholder="Email"
+                            className="w-full mt-3 p-3 border rounded-xl bg-gray-50" />
+                        <label className="uppercase text-gray-600 block text-lx font-bold">Password</label>
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            className="w-full mt-3 p-3 border rounded-xl bg-gray-50" />
+                    </div>
+                    <input
+                        type="submit"
+                        value="Iniciar Sesion"
+                        className="bg-blue-900 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-zinc-800 transition-colors" />
+
+                    <Link
+                        className="block text-center my-5"
+                        to={"/crear-cuenta"}
+                    >
+                        Crear Cuenta
+                    </Link>
+                </form>
+            </div>
+        </main>
     );
 }
 
